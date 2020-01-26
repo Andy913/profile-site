8e26634261045205da26c6b216878eef8d453160
AndyH <andyh1393@gmail.com>
2020-01-26 11:08:56 +1100

    [api] Added sequelize

--- api/src/index.js
+++ api/src/index.js
@@ -1,4 +1,16 @@
 import app from './app';
+import sequelize from './models/sequelize'
 
 const { PORT = 8080 } = process.env;
+
+// Added for debugging purposes, to be removed later
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`)); // eslint-disable-line no-console
