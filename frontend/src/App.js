51408ffe86772fb8d89bec5225a696c9adeaba73
Andy913 <andyh1393@gmail.com>
2020-02-09 08:55:11 +1100

    [frontend] Added basic page routing (#1)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div>Hello World</div>
-  );
+// Routes
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import About from "./pages/About"
+
+class App extends React.Component {
+    render() {
+        return (
+            <>
+                <Router>
+                    <div>
+                        <Route exact path='/' component={Home}/>
+                        <Route path='/contact' component={Contact}/>
+                        <Route path='/about' component={About}/>
+                    </div>
+                </Router>
+            </>
+        )
+    }
 }
 
 export default App;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div style={{ color: `purple` }}>
+                <h1>Andy Ho</h1>
+                <img src="https://source.unsplash.com/random/400x200" alt="" />
+          </div>
+        )
+    }
+}
+
+export default Home;
