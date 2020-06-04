012a87fb1f3b0a1bec02e75c483298286822a0de
AndyH <andyh1393@gmail.com>
2020-06-04 12:08:26 +1000

    Committing cleanup changes

--- db/README.md
+++ db/README.md
@@ -14,6 +14,8 @@ Connect to the database
 
 ```docker exec -it profile_site_postgres psql -d profile_site_db -U admin```
 
+https://support.rackspace.com/how-to/postgresql-creating-and-dropping-roles/
+
 Stop the container
 
 ```docker-compose down```
--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 class About extends React.Component {
     render() {
         return (
-            <div>About page</div>
+            <>
+                <div>About page</div>
+                <Container>
+                    <Row>
+                        <Col>1 of 2</Col>
+                        <Col>2 of 2</Col>
+                    </Row>
+                    <Row>
+                        <Col>1 of 3</Col>
+                        <Col>2 of 3</Col>
+                        <Col>3 of 3</Col>
+                    </Row>
+                </Container>
+            </>
         )
     }
 }
